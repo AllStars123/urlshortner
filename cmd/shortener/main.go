18feec34a57c1e9c36ab598611b744eafa6cf084
Никита <nikita@MacBook-Air-Nikita.local>
2022-09-06 00:50:49 +0300

    fix tests

--- cmd/shortener/main.go
+++ cmd/shortener/main.go
@@ -10,7 +10,7 @@ import (
 func main() {
 	router := gin.Default()
 	data := url.Values{}
-	router.POST("/post", handlers.CreateShortURL(data))
+	router.POST("/", handlers.CreateShortURL(data))
 	router.GET("/:id", handlers.RetriveShortURL(data))
 	http.ListenAndServe(":8080", router)
 }
