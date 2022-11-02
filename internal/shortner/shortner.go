3c3d4f227d97e2fe5d43d964c3ba712bda045776
Никита <nikita@MacBook-Air-Nikita.local>
2022-11-03 01:58:34 +0300

    test new func for short

--- internal/shortner/shortner.go
+++ internal/shortner/shortner.go
@@ -3,11 +3,31 @@ package shortner
 import (
 	"crypto/sha1"
 	"encoding/base64"
+	"strings"
 )
 
 func Shorten(longURL string) string {
+	splitURL := strings.Split(longURL, "://")
 	hasher := sha1.New()
-	hasher.Write([]byte(longURL))
-	urlHash := base64.URLEncoding.EncodeToString(hasher.Sum(nil))[:10]
-	return urlHash
+	if len(splitURL) < 2 {
+		hasher.Write([]byte(longURL))
+	} else {
+		hasher.Write([]byte(splitURL[1]))
+	}
+	urlHash := base64.URLEncoding.EncodeToString(hasher.Sum(nil))
+	return string(urlHash)
 }
+
+//package shortner
+//
+//import (
+//"crypto/sha1"
+//"encoding/base64"
+//)
+//
+//func Shorten(longURL string) string {
+//	hasher := sha1.New()
+//	hasher.Write([]byte(longURL))
+//	urlHash := base64.URLEncoding.EncodeToString(hasher.Sum(nil))[:10]
+//	return urlHash
+//}
